7ba1c048e94479ee1658ad418bcad305d319dfc3
joelito24 <joelsoulange@gmail.com>
2023-05-23 16:46:25 +0200

    Feat (ultimo commit)

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -6,51 +6,67 @@
         <div class="col-md-12">
             <a href="{{ route('products.create') }}" class="btn btn-primary">Crear producto</a>
             <a href="{{ route('proveedores.create') }}" class="btn btn-primary">Crear proveedor</a>
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>proveedor</th>
-                        <th>Descripción</th>
-                        <th>Precio Unidad</th>
-                        <th>Precio Subtotal</th>
-                        <th>Cantidad</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($products as $key => $product)
-                    <tr>
-                        <td>{{ $product->id }}</td>
-                        <td>{{ $product->name }}</td>
-                        <td>{{ $product->options->proveedor }}</td>
-                        <td>{{ $product->options->description }}</td>
-                        <td>{{ $product->price }}</td>
-                        <td>{{ $product->price * $product->qty }}</td>
-                        <td>
-                            {{ $product->qty }}
-                        </td>
-                        <td>
-                            <form action="{{ route('cart.remove', $product->rowId) }}" method="POST">
-                                @csrf
-                                <button class="btn btn-danger">Eliminar</button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            <div class="rowjustify-content-end">
-                <div class="col-md-12">
-                    <p class="text-center" style="font-size: 20px; font-weight: bold; text-align-last: right;">Total: {{ Cart::Total() }}</p>
+            @if ($products->isEmpty())
+                <div class="alert alert-info" role="alert" style="margin-top: 20px;">
+                    El carrito está vacío.
                 </div>
-            </div>
-            <div class="row" style="text-align: right;">
-                <div class="col-md-12">
-                    <button class="btn btn-success" id="btnComprar" onclick="submitComprado()">Comprar</button>
+                <div class="row justify-content-end">
+                    <div class="col-md-12">
+                        <p class="text-center" style="font-size: 20px; font-weight: bold; text-align-last: right;">Total: {{ Cart::Total() }}</p>
+                    </div>
                 </div>
-            </div>
+                <div class="row" style="text-align: right;">
+                    <div class="col-md-12">
+                        <button class="btn btn-danger" id="btnComprar-vacio">Comprar</button>
+                    </div>
+                </div>
+            @else
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>proveedor</th>
+                            <th>Descripción</th>
+                            <th>Precio Unidad</th>
+                            <th>Precio Subtotal</th>
+                            <th>Cantidad</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($products as $key => $product)
+                        <tr>
+                            <td>{{ $product->id }}</td>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->options->proveedor }}</td>
+                            <td>{{ $product->options->description }}</td>
+                            <td>{{ $product->price }}</td>
+                            <td>{{ $product->price * $product->qty }}</td>
+                            <td>
+                                {{ $product->qty }}
+                            </td>
+                            <td>
+                                <form action="{{ route('cart.remove', $product->rowId) }}" method="POST">
+                                    @csrf
+                                    <button class="btn btn-danger">Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <div class="row justify-content-end">
+                    <div class="col-md-12">
+                        <p class="text-center" style="font-size: 20px; font-weight: bold; text-align-last: right;">Total: {{ Cart::Total() }}</p>
+                    </div>
+                </div>
+                <div class="row" style="text-align: right;">
+                    <div class="col-md-12">
+                        <button class="btn btn-success" id="btnComprar" onclick="submitComprado()">Comprar</button>
+                    </div>
+                </div>
+            @endif
         </div>  
     </div>
 </div>
@@ -58,12 +74,7 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script>
-    function submitComprado(){
+    function submitComprado() {
         window.location.href = "{{ route('confirmacion-compra') }}";
     }
 </script>
-
-    
-
-
-                  
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    
+<div class="container-fluid" style="background: url(../img/bgpro.jpg) no-repeat center center fixed; background-size: cover; height: 100vh;">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="jumbotron text-center" style="background-color: darkblue; color: white;">
+                <h1>Sule's Shop</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container" style="margin-top: 90px;">
+        <div class="row justify-content-center">
+            <div class="col-md-2">
+                <a href="{{ route('cart.index') }}" class="btn btn-primary btn-lg btn-block" style="background-color: darkblue;">Carrito</a>
+            </div>
+            <div class="col-md-2">
+                <a href="{{ route('products') }}" class="btn btn-primary btn-lg btn-block" style="background-color: darkblue;">Productos</a>
+            </div>
+            <div class="col-md-2">
+                <a href="{{ route('proveedores.create') }}" class="btn btn-primary btn-lg btn-block" style="background-color: darkblue;">Proveedores</a>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
